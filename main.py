3c8d629bc5cc45032830dc255c9271f7dc39b863
Bohdan Sizomin <nbzp28@gmail.com>
2022-08-20 15:58:43 +0300

    Minor updates

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 # getting user data generation
-username = str(input('Please,insert your username:\n')) \
-    + random.choice("abcdefghijklmnopqrstuvwxyz1234567890") \
-    + random.choice("abcdefghijklmnopqrstuvwxyz1234567890") \
-    + random.choice("abcdefghijklmnopqrstuvwxyz1234567890")
-email = username + '@testgmail.com'
+username = str(input('Please,insert your username:\n'))
+email = str(input('Please,insert your email:\n'))
+
+if "@" not in email:
+    print("@ is missing in email\n")
+    exit()
 day = str(random.randint(1, 28))
 password = passgen.passgen(
     length=12, punctuation=False, digits=True, letters=True, case="both")
@@ -28,15 +29,18 @@ actions = ActionChains(driver)
 driver.get("https://discord.com/register")
 
 
-months = ['//*[@id="react-select-2-option-0"]', '//*[@id="react-select-2-option-1"]', '//*[@id="react-select-2-option-2"]', '//*[@id="react-select-2-option-3"]', '//*[@id="react-select-2-option-4"]', '//*[@id="react-select-2-option-5"]',
-          '//*[@id="react-select-2-option-6"]', '//*[@id="react-select-2-option-7"]', '//*[@id="react-select-2-option-8"]', '//*[@id="react-select-2-option-9"]', '//*[@id="react-select-2-option-10"]', '//*[@id="react-select-2-option-11"]']
-
+months = []
 days = []
+years = []
+
+for i in range(0, 11):
+    month = f'//*[@id="react-select-2-option-{i}"]'
+    months.append(month)
+
 for i in range(1, 28):
     day = f'//*[@id="react-select-3-option-{i}"]'
     days.append(day)
 
-years = []
 for i in range(19, 32):
     year = f'//*[@id="react-select-4-option-{i}"]'
     years.append(year)
@@ -75,16 +79,16 @@ driver.find_element(By.CSS_SELECTOR, "[type=submit]").click()
 time.sleep(30)
 
 
+input('Please,solve captcha')
 
-input('Please,solve captcha') 
-
-#Getting 
+# Getting
 token = driver.execute_script(
-  'location.reload(); \
+    'location.reload(); \
   var i=document.createElement("iframe"); \
   document.body.appendChild(i); \
   return i.contentWindow.localStorage.token').split('""')
 
-print(f'Username is - {username}\n Email is - {email}\n Password is {password}\n Token is {token}')
+print(
+    f'Username is - {username}\n Email is - {email}\n Password is {password}\n Token is {token}')
 
 actions.perform()
